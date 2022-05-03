70cd1a7e5895195fe29ea76c67db8872b5a31597
inusturbo <shanpeng.ma@gmail.com>
2022-05-03 15:44:34 +0900

    finish 1.3

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -14,9 +14,7 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part} exercises={props.exercises} />
-      <Part part={props.part} exercises={props.exercises} />
-      <Part part={props.part} exercises={props.exercises} />
+      <Part part={props.part.name} exercises={props.part.exercises} />
     </div>
   )
 }
@@ -29,21 +27,37 @@ const Total = (props) => {
 
 
 const App = () => {
+  // const course = 'Half Stack application development'
+  // const part1 = 'Fundamentals of React'
+  // const exercises1 = 10
+  // const part2 = 'Using props to pass data'
+  // const exercises2 = 7
+  // const part3 = 'State of a component'
+  // const exercises3 = 14
+
+
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
 
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Content part={part1} />
+      <Content part={part2} />
+      <Content part={part3} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   )
 }
